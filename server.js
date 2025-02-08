e1954f0e3baef42f83596e0aa5332aeba670dd37
Alexey Borisov <ablogmount@yandex.ru>
2025-02-08 22:13:57 +0500

    Add

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const getIngredients = require('./src/controllers/ingredients');
-const ingredients = require('./src/controllers/ingredients');
-const getOrders = require('./src/controllers/orders/all'); 
-const getAuth = require('./src/controllers/auth/login');
-const getRegister = require('./src/controllers/auth/register');
+const ingredients = require('./src/controllers/ingredients'); // Импортируем данные
 
 // Создаем экземпляр приложения
 const app = express();
@@ -15,18 +11,19 @@ app.use(express.static(path.join(__dirname, 'src', 'views')));
 
 // Обработка маршрутов
 app.get('/api/ingredients', (req, res) => {
-  res.json(ingredients); 
+    res.json(ingredients); // Возвращаем данные напрямую
 });
+// Другие маршруты
 app.get('/api/orders/all', getOrders); 
 app.post('/api/auth/login', getAuth);
 app.post('/api/auth/register', getRegister);
 
 // Главная страница
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'));
 });
 
 // Запуск сервера
 app.listen(PORT, () => {
-    console.log(`Сервер запущен на https://api-constructor.onrender.com/`);
+    console.log(`Сервер запущен на https://api-constructor.onrender.com/api/`);
 });
