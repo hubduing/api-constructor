a5d00fee6952970bc93a1f874a922d447ef6b7fd
Alexey Borisov <ablogmount@yandex.ru>
2025-02-09 12:07:57 +0500

    Add

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'src', 'views')));
 
-app.get('/api/ingredients', getIngredients);
-app.get('/api/orders/all', getOrders);
-app.post('/api/orders', createOrder);
-app.post('/api/auth/login', getAuth);
-app.post('/api/auth/register', getRegister);
+app.get('/ingredients', getIngredients);
+app.get('/orders/all', getOrders);
+app.post('/orders', createOrder);
+app.post('/auth/login', getAuth);
+app.post('/auth/register', getRegister);
 
-app.get('/api/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Сервер запущен на https://api-constructor.onrender.com/api/`);
+    console.log(`Сервер запущен на https://api-constructor.onrender.com/`);
 });
